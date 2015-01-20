9569a47e01c1b50832221c1595552611b019d8c5
alyssako <alyssako@eecs373-10.eecs.umich.edu>
2015-01-20 02:06:51 -0500

    drunken robot

--- src/a0/maebot_motor_gui.c
+++ src/a0/maebot_motor_gui.c
@@ -40,11 +40,22 @@ typedef struct
     float theta;
 } odometry_t;
 
+typedef struct
+{
+    float theta;
+    float v_xr;
+    float px;
+    float py;
+} imu_t;
+
+imu_t *imu_data;
+int imu_data_size;
 odometry_t *odometry_data;
+int odometry_data_size;
 int left_tick_start, right_tick_start;
 
 //function to calculate the distance
-void calc_move(int sL, int sR, int index, FILE*fp){
+void calc_move(int sL, int sR, int index){
 	float dtheta, s, alpha;
     float x_prev = odometry_data[index-1].x;
     float y_prev = odometry_data[index-1].y;
@@ -52,7 +63,6 @@ void calc_move(int sL, int sR, int index, FILE*fp){
 
     int sL_i = sL - left_tick_start;
     int sR_i = sR - right_tick_start;
-    fprintf(fp, "%d %d\n", sL_i, sR_i);
 
     // increment left and right starting tick
     left_tick_start = sL;
@@ -61,12 +71,10 @@ void calc_move(int sL, int sR, int index, FILE*fp){
     //convert ticks to meters
     float sL_f = sL_i/4800.0;
     float sR_f = sR_i/4800.0;
-    fprintf(fp, "%f %f\n", sL_f, sR_f);
 
     s = (sR_f + sL_f)/2;
     dtheta = (sR_f - sL_f)/0.08;
     alpha = dtheta/2;
-    fprintf(fp, "%f %f %f\n", s, dtheta, alpha);
     odometry_data[index].x = s*(cosf(theta_prev + alpha)) + x_prev;
     odometry_data[index].y = s*(sinf(theta_prev + alpha)) + y_prev;
     odometry_data[index].theta = dtheta + theta_prev;
@@ -84,10 +92,10 @@ static void get_odometry_data()
     }
     
     FILE *ofp;
-    ofp = fopen("output.txt", "w");
+    ofp = fopen("output_odometer.txt", "w");
     if(ofp == NULL)
     {
-        printf("Can't open output.txt!\n");
+        printf("Can't open output_odometer.txt!\n");
         exit(1);
     }
     
@@ -107,23 +115,80 @@ static void get_odometry_data()
 
     while(fscanf(fp, "%d %d", &left_tick, &right_tick) == 2)
     {
-        calc_move(left_tick, right_tick, index, ofp);
-        fprintf(ofp, "%f %f %f\n\n", odometry_data[index].x, odometry_data[index].y, odometry_data[index].theta);
+        calc_move(left_tick, right_tick, index);
+        fprintf(ofp, "%f %f %f\n", odometry_data[index].x, odometry_data[index].y, odometry_data[index].theta);
         index++;
     }
-    printf("final index: %d", index);
+    odometry_data_size = index;
+}
+
+static void calc_move_acc(long long delta_timestamp, int x_acc, int gyro_z, int index)
+{
+    imu_data[index].theta = imu_data[index-1].theta + (gyro_z/131.0)*delta_timestamp/1000000.0;
+    imu_data[index].v_xr = imu_data[index-1].v_xr + (x_acc/16384.0)*delta_timestamp/1000000.0;
+    float v_xw = imu_data[index].v_xr*cosf(imu_data[index].theta);
+    float v_yw = imu_data[index].v_xr*sinf(imu_data[index].theta);
+    imu_data[index].px = imu_data[index-1].px + v_xw*delta_timestamp/1000000.0;
+    imu_data[index].py = imu_data[index-1].py + v_yw*delta_timestamp/1000000.0;
+}
+
+static void get_imu_data()
+{
+    long long timestamp;
+    int x_acc, y_acc, z_acc, gyro_x, gyro_y, gyro_z;
+    FILE *fp;
+    imu_data = calloc(100000, sizeof(imu_t));
+    fp = fopen("real_movement_data.txt", "r");
+    if(fp == NULL)
+    {
+        printf("Can't open real_movement_data.txt!\n");
+        exit(1);
+    }
+
+    FILE *ofp;
+    ofp = fopen("output.txt", "w");
+    if(ofp == NULL)
+    {
+        printf("Can't open output.txt\n");
+        exit(1);
+    }
+
+    int index = 0;
+    imu_data[index].theta = 0;
+    imu_data[index].v_xr = 0;
+    imu_data[index].px = 0;
+    imu_data[index].py = 0;
+    index++;
+
+    fprintf(ofp, "%lld %f %f %f %f\n", 0, imu_data[index].theta, imu_data[index].v_xr, imu_data[index].px, imu_data[index].py);
+    int first = fscanf(fp, "%lld %d %d %d %d %d %d", &timestamp, &x_acc, &y_acc, &z_acc, &gyro_x, &gyro_y, &gyro_z);
+    if(first != 7)
+    {
+        printf("error reading real_movement_data.txt\n");
+        exit(1);
+    }
+    unsigned long long prev_timestamp = timestamp;
+
+    while(fscanf(fp, "%lld %d %d %d %d %d %d", &timestamp, &x_acc, &y_acc, &z_acc, &gyro_x, &gyro_y, &gyro_z) == 7)
+    {
+        calc_move_acc(timestamp - prev_timestamp, x_acc, gyro_z, index);
+        fprintf(ofp, "%lld %f %f %f %f\n", (timestamp - prev_timestamp), imu_data[index].theta, imu_data[index].v_xr, imu_data[index].px, imu_data[index].py);
+        index++;
+        prev_timestamp = timestamp;
+    }
+    imu_data_size = index;
 }
 
 static void draw(state_t * state, vx_world_t * world)
 {
     // draw origin
-    float x1=1, x_1=-1, y1=1, y_1=-1, z0=0, x0=0, y0=0;
+    /*float x1=1, x_1=-1, y1=1, y_1=-1, z0=0, x0=0, y0=0;
     float origin_pts[] = {x0, y_1, z0, x0, y1, z0, x_1, y0, z0, x1, y0, z0};
     int origin_pts_size = 4;
     vx_resc_t *origin_verts = vx_resc_copyf(origin_pts, origin_pts_size*3);
     vx_buffer_add_back(vx_world_get_buffer(world, "origin"), vxo_lines(origin_verts, origin_pts_size, GL_LINES, vxo_lines_style(vx_red, 2.0f)));
     vx_buffer_swap(vx_world_get_buffer(world, "origin"));
-
+    */
     // make legend
     int lgnd_pts = 2;
     vx_object_t *lgnd = vxo_text_create(VXO_TEXT_ANCHOR_CENTER, "<<middle, #000000>> Legend\n");
@@ -138,99 +203,12 @@ static void draw(state_t * state, vx_world_t * world)
     vx_buffer_add_back(wrld, vxo_pix_coords(VX_ORIGIN_CENTER, vxo_chain(vxo_mat_translate2(100, 180), vxo_mat_scale(0.8), lgnd)));
     vx_buffer_add_back(wrld, vxo_pix_coords(VX_ORIGIN_CENTER, vxo_chain(vxo_mat_translate2(100, 165), vxo_mat_scale(0.6), odm)));
     vx_buffer_add_back(wrld, vxo_pix_coords(VX_ORIGIN_CENTER, vxo_chain(vxo_mat_translate2(140, 165), vxo_mat_scale(1.0), 
-			     vxo_lines(odm_verts, lgnd_pts, GL_LINES, vxo_points_style(vx_red, 2.0f))))); 
+			     vxo_lines(odm_verts, lgnd_pts, GL_LINES, vxo_points_style(vx_blue, 2.0f))))); 
     vx_buffer_add_back(wrld, vxo_pix_coords(VX_ORIGIN_CENTER, vxo_chain(vxo_mat_translate2(117, 150), vxo_mat_scale(0.6), imu)));
     vx_buffer_add_back(wrld, vxo_pix_coords(VX_ORIGIN_CENTER, vxo_chain(vxo_mat_translate2(140, 150), vxo_mat_scale(1.0), 
-			     vxo_lines(imu_verts, lgnd_pts, GL_LINES, vxo_points_style(vx_blue, 2.0f)))));
+			     vxo_lines(imu_verts, lgnd_pts, GL_LINES, vxo_points_style(vx_red, 2.0f)))));
     vx_buffer_swap(wrld);
     
-   
-    
-
-    // Draw from the vx shape library
-    /*if (1) {
-        vx_buffer_add_back(vx_world_get_buffer(world, "fixed-cube"),
-                           vxo_chain(vxo_mat_translate3(3.0,0,0),
-                                     vxo_mat_scale3(2,2,2),
-                                     // vxo_box(vxo_mesh_style(vx_orange)))); 
-                                     vxo_box(vxo_mesh_style_fancy(vx_orange, vx_orange, vx_white, 1.0, 400.0, 2))));
-
-
-        vx_buffer_add_back(vx_world_get_buffer(world, "fixed-cube"),
-                           vxo_chaiwhile (fscanf(ifp, "%s %d", username, &score) == 2) {n(vxo_mat_translate3(0,3.0,0),
-                                     vxo_mat_scale3(1,1,1),
-                                     vxo_depth_test(0,
-                                                    vxo_box(vxo_mesh_style_solid(vx_green)))));
-        vx_buffer_swap(vx_world_get_buffer(world, "fixed-cube"));
-    }
-
-    if (1) {
-        float Tr = .2;
-        float amb[] = {0.0,0.0,0.0};
-        float diff[] = {0.0,0.0,0.0};
-        float spec[] = {1.0,1.0,1.0};
-        float specularity = 1.0;
-
-        int type = 2;
-
-        vx_buffer_add_back(vx_world_get_buffer(world, "window"),
-                           vxo_chain(vxo_mat_translate3(0,0,2.5),
-                                     vxo_mat_rotate_y(-M_PI/7),
-                                     vxo_mat_rotate_z(M_PI/5),
-                                     vxo_mat_rotate_x(M_PI/2),
-                                     vxo_mat_scale3(10,10,1),
-                                     vxo_rect(vxo_mesh_style_fancy(amb, diff, spec, Tr, specularity, type),
-                                              vxo_lines_style(vx_black,2))));
-        vx_buffer_swap(vx_world_get_buffer(world, "window"));
-        vx_buffer_set_draw_order(vx_world_get_buffer(world, "window"), 100);
-    }
-
-
-    if (1) {
-        // Draw a custom ellipse:
-        int npoints = 35;
-        float points[npoints*3];
-        for (int i = 0; i < npoints; i++) {
-            float angle = 2*M_PI*i/npoints;
-
-            float x = 5.0f*cosf(angle);
-            float y = 8.0f*sinf(angle);
-            float z = 0.0f;
-
-            points[3*i + 0] = x;
-            points[3*i + 1] = y;
-            points[3*i + 2] = z;
-        }
-
-        vx_buffer_add_back(vx_world_get_buffer(world, "ellipse"), vxo_lines(vx_resc_copyf (points, npoints*3),
-                                                                            npoints, GL_LINE_LOOP,
-                                                                            vxo_lines_style(vx_purple, 1.0f) ));
-        vx_buffer_swap(vx_world_get_buffer(world, "ellipse"));
-    }
-
-    if (1) {
-        vx_object_t *vt = vxo_text_create(VXO_TEXT_ANCHOR_TOP_RIGHT, "<<right,#0000ff>>Heads Up!\n");
-        vx_buffer_t *vb = vx_world_get_buffer(world, "text");
-        vx_buffer_add_back(vb, vxo_pix_coords(VX_ORIGIN_TOP_RIGHT,vt));
-        vx_buffer_swap(vb);
-    }
-
-    // Draw a texture
-    if (state->img != NULL){
-        image_u32_t * img = state->img;
-        vx_object_t * o3 = vxo_image_texflags(vx_resc_copyui(img->buf, img->stride*img->height),
-                                              img->width, img->height, img->stride,
-                                              GL_RGBA, VXO_IMAGE_FLIPY,
-                                              VX_TEX_MIN_FILTER | VX_TEX_MAG_FILTER);
-
-        // pack the image into the unit square
-        vx_buffer_t * vb = vx_world_get_buffer(world, "texture");
-        vx_buffer_add_back(vb,vxo_chain(
-                               vxo_mat_scale(1.0/img->height),
-                               vxo_mat_translate3(0, - img->height, 0),
-                                        o3));
-        vx_buffer_swap(vb);
-    }*/
 }
 
 static void display_finished(vx_application_t * app, vx_display_t * disp)
@@ -297,15 +275,34 @@ static state_t * state_create()
 void * render_loop(void * data)
 {
     state_t * state = data;
+    int count = 0;
     while(state->running) {
-        double rad = (vx_util_mtime() % 5000) * 2* M_PI / 5e3;
-
-        vx_object_t * vo = vxo_chain(vxo_mat_rotate_z(rad),
-                                     vxo_mat_translate2(0,10),
-                                     vxo_box(vxo_mesh_style(vx_blue)));
-
-        vx_buffer_add_back(vx_world_get_buffer(state->world, "rotating-square"), vo);
-        vx_buffer_swap(vx_world_get_buffer(state->world, "rotating-square"));
+        int i=0;
+        vx_buffer_t *buf = vx_world_get_buffer(state->world, "odometer");
+        
+        while(i < count-1 && i < imu_data_size-1 && count < imu_data_size)
+        {
+            float pts[] = {imu_data[i].px*15, imu_data[i].py*15, 0,
+                           imu_data[i+1].px*15, imu_data[i+1].py*15, 0};
+            vx_resc_t *verts = vx_resc_copyf(pts, 6);
+            vx_buffer_add_back(buf, vxo_lines(verts, 2, GL_LINES, vxo_lines_style(vx_red, 2.0f)));
+            i++;
+        }
+        i = 0;
+        while(i < count-1 && i < odometry_data_size-1 && count < odometry_data_size)
+        {
+            float pts[] = {odometry_data[i].x*15, odometry_data[i].y*15, 0,
+                           odometry_data[i+1].x*15, odometry_data[i+1].y*15, 0};
+            vx_resc_t *verts = vx_resc_copyf(pts, 6);
+            vx_buffer_add_back(buf, vxo_lines(verts, 2, GL_LINES, vxo_lines_style(vx_blue, 2.0f)));
+            i++;
+        }
+        count++;
+        if(count == imu_data_size || count == odometry_data_size)
+        {
+            count--;
+        }
+        vx_buffer_swap(buf);
         usleep(5000);
     }
 
@@ -315,6 +312,7 @@ void * render_loop(void * data)
 int main(int argc, char ** argv)
 {
     get_odometry_data();
+    get_imu_data();
 
     getopt_t *gopt = getopt_create();
     getopt_add_bool   (gopt, 'h', "help", 0, "Show help");
